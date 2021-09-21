f48465787b1568c5864006ed087d508ddc1ec926
John Spencer <jespence9901@gmail.com>
2021-09-21 18:59:37 -0400

    fixed change with component annotation

--- java/src/main/java/com/techelevator/services/YelpService.java
+++ java/src/main/java/com/techelevator/services/YelpService.java
@@ -8,11 +8,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class YelpService {
 
     private String apiURL = "https://api.yelp.com/v3/businesses/search?&limit=10";
