80cc772b3e758363b85ba3b2d9f15be85124eca4
Arshpreet Aulakh <arshpreetsingh5211@gmail.com>
2021-09-24 13:51:15 -0400

    can't figure out how to align home page

--- vue/src/components/RestaurantSearch.vue
+++ vue/src/components/RestaurantSearch.vue
@@ -1,10 +1,7 @@
 <template>
-  <div>
-
-      <div class='container'>
-      <!-- <div class='restaurant-card' v-for="restaurant in restaurants" v-bind:key="restaurant.id"> -->
-        <table>
-            <thead>
+  <div class="search-page">
+        <table class='table-container'>
+            <thead class="table-headers">
                 <tr>
                     <th>Restaurant Name</th>
                     <th>Phone Number</th>
@@ -19,11 +16,13 @@
                   </tr>
               </tbody>
           </table>
-      </div>
-
-      <input v-model="preference" type="text" name="test" id="">
-      <input v-model="location" type="text">
+    <div class = "search-bar">
+      <label for="category">What are you in the mood for?</label>
+      <input v-model="preference" type="text" name="test" id="" placeholder="Pizza"/>
+      <label for="zip-code"><br>Which zip code?</label>
+      <input v-model="location" type="text" placeholder="19210"/>
       <button v-on:click="startSearch" >Search</button>
+    </div>
   </div>
 </template>
 
@@ -54,14 +53,34 @@ export default {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond&family=Montserrat&family=Roboto:wght@300&display=swap');
 
-.restaurant-card {
-    border: 1px solid black;
-    background: whitesmoke;
+.search-page {
+    width: 100%;
+    height: 100%;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+}
+
+.search-bar label {
+    align-content: center;
+    padding: 0 0 100px 0;
+}
+
+.search-bar {
+    display: block;
+    margin: 0;
+    position: absolute;
+    top: 15%;
+    left: 60%;
+    font-family: 'Montserrat', serif;
+    font-size: 12px;
+    background:whitesmoke;
+    padding: 10px 40px 20px 40px;
+    text-align: center;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
     border-radius: 10px;
-    width: 250px;
-    height: 200px;
-    margin: 20px;
-    padding-left: 10px;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
 }
 
 .restaurant-name {
@@ -83,24 +102,31 @@ export default {
     line-height: 27.2px;
 }
 
-.restaurant-phone {
-    font-family: 'Montserrat', serif;
-
+.table-headers {
+    font-family: 'Cormorant Garamond', serif;
+    text-transform: uppercase;
 }
 
-.container {
+.restaurant-phone {
+    font-family: 'Montserrat', serif;
+    color: #42403ff1;
+}
+
+.table-container {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
     margin: 0;
     position: absolute;
-    top: 40%;
-    left: 30%;
+    top: 30%;
+    left: 20%;
     border: 1px solid black;
     background: whitesmoke;
     border-radius: 10px;
     padding: 20px;
-    height: 60%;
+    max-height: 30%;
     overflow-y: scroll;
 }
 
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <!-- <h1>Home</h1> -->
     <restaurant-search></restaurant-search>
-    <p>You must be authenticated to see this</p>
+    <!-- <p>You must be authenticated to see this</p> -->
     <restaurant-card></restaurant-card>
   </div>
 </template>
