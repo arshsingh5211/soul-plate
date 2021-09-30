bf70f7b7622cc7f6e48e76cb1c49dd70692b08d6
Arshpreet Aulakh <arshpreetsingh5211@gmail.com>
2021-09-30 11:10:54 -0400

    added authentication and changed saveLiked endpoint

--- java/src/main/java/com/techelevator/controller/RestaurantController.java
+++ java/src/main/java/com/techelevator/controller/RestaurantController.java
@@ -7,6 +7,7 @@ import com.techelevator.model.*;
 import com.techelevator.services.YelpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -25,16 +26,19 @@ public class RestaurantController {
     @Autowired
     UserDao userDao;
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @RequestMapping(path="/restaurants", method = RequestMethod.GET)
     public List<Restaurant> listSearchResults(@RequestParam String foodPref, @RequestParam String location) {
         return yelpService.getSearchResults(foodPref, location);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @RequestMapping(path="/restaurants/{id}", method = RequestMethod.GET)
     public RestaurantDetails getRestaurantDetails(@PathVariable String id) {
         return yelpService.getRestaurantDetails(id);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @RequestMapping(path = "/restaurant", method = RequestMethod.GET)
     public Restaurant getRandomRestaurant(@RequestParam String foodPref, @RequestParam String location) {
         return yelpService.getRandomRestaurant(yelpService.getSearchResults(foodPref, location));
@@ -42,19 +46,22 @@ public class RestaurantController {
 
 
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ROLE_USER')")
     @RequestMapping(path = "/liked", method = RequestMethod.POST)
     public void addLikedRestaurant(@Valid @RequestBody Restaurant restaurant, Principal principal) {
-        System.out.println(principal.getName());
+        System.out.println(principal.toString());
         String user = principal.getName();
         int id = userDao.findIdByUsername(user);
         restaurantDAO.saveLikedRestaurant(restaurant, id);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @RequestMapping(path = "/{id}/liked", method = RequestMethod.GET)
     public List<Restaurant> getLikedRestaurants(@PathVariable int id) {
         return restaurantDAO.getLikedRestaurants(id);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @RequestMapping(path = "/restaurants/{id}/reviews", method = RequestMethod.GET)
     public List<ReviewUser> getReview(@PathVariable String id) {
         return yelpService.getReview(id);
