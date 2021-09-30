4ff5244603edea6f6c41b56f47590ada93f6c099
erva taskin <ervataskin123@gmail.com>
2021-09-30 19:02:19 -0400

    address is centered on the card

--- vue/src/components/DisplayRestaurants.vue
+++ vue/src/components/DisplayRestaurants.vue
@@ -303,7 +303,7 @@ input {
   align-content: center;
 }
 h3 {
-  background: lightgrey;
+  
   width: 200px;
   align-content: center;
   text-align: center;
@@ -339,5 +339,11 @@ h3 {
 }
 button {
   vertical-align: baseline;
+}
+
+.restaurant-address {
+  display: inline;
+  align-items: center;
+  
 }
 </style>
