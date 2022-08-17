2453201cadcf527320ac70cb32043380d888c503
arsh <arshsingh5211@gmail.com>
2022-08-16 20:52:15 -0400

    gotta make things worse before i make them better

--- java/src/main/java/com/techelevator/dao/JDBCPreferenceDAO.java
+++ java/src/main/java/com/techelevator/dao/JDBCPreferenceDAO.java
@@ -16,19 +16,19 @@ public class JDBCPreferenceDAO implements PreferenceDAO {
     }
 
     @Override
-    public Preferences getUserPreferences(int preferencesId) {
-        Preferences preferences = null;
-        String sql = "SELECT * FROM user_preferences WHERE preferences_id = ?;";
-        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, preferencesId);
-        if (results.next()) {
-            preferences = mapRowToUserPreferences(results);
+    public List<Preferences> getUserPreferences(int userId) {
+        List<Preferences> preferencesList = new ArrayList<>();
+        String sql = "SELECT name, home_zip, preference_id, preference FROM preferences JOIN user_preferences USING (preferences_id) " +
+                "JOIN users USING (user_id) WHERE user_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, userId);
+        while (results.next()) {
+            preferencesList.add(mapRowToUserPreferences(results));
         }
-        return preferences;
+        return preferencesList;
     }
 
     @Override
     public List<Preferences> getPreferencesByUserId(int userId) {
-
         List<Preferences> preferencesList = new ArrayList<>();
         String sql = "SELECT * FROM user_preferences WHERE user_id = ?";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, userId);
@@ -50,7 +50,7 @@ public class JDBCPreferenceDAO implements PreferenceDAO {
         String query2 = "INSERT INTO user_preferences (user_id, preferences_id, name, home_zip) VALUES (" +
                 "(SELECT user_id FROM users WHERE user_id = ?), (SELECT preferences_id FROM preferences WHERE " +
                 "preferences_id = ?), ?, ?)";
-        jdbcTemplate.update(query2, userId, prefId, newPreferences.getName(), newPreferences.getHomeZip());
+        jdbcTemplate.update(query2, userId, prefId);
     }
 
     // allow users to add more preferences not just replace when you choose new pref
--- java/src/main/java/com/techelevator/dao/PreferenceDAO.java
+++ java/src/main/java/com/techelevator/dao/PreferenceDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface PreferenceDAO {
 
-     public Preferences getUserPreferences(int preferenceId);
+     public List<Preferences>  getUserPreferences(int userId);
 
      public List<Preferences> getPreferencesByUserId(int userId);
 
